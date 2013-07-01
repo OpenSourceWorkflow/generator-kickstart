fff5658d5eacb08425ae594c97d32640026ed9cd
Markus Falk <falk@webit.de>
2013-07-01 12:32:23 +0200

    [Task] remove timestamp because of yeoman template()

--- generator-kickstart/app/templates/_gruntfile.js
+++ generator-kickstart/app/templates/_gruntfile.js
@@ -3,8 +3,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    timestamp: 'test',
-
+    
     compass: {
       dist: {
         options: {
