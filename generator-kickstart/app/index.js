92d557604976b40737ae8d6b617eb19023dc6d69
Markus Falk <post@markus-falk.com>
2013-07-04 21:58:57 +0200

    [Task] remove test

--- generator-kickstart/app/index.js
+++ generator-kickstart/app/index.js
@@ -20,7 +20,6 @@ KickstartGenerator.prototype.askFor = function askFor() {
 
   // have Yeoman greet the user.
   console.log(this.yeoman);
-  console.log('test');
 
   var prompts = [{
     name: 'projectName',
