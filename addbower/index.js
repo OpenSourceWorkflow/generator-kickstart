9797ebdd5d8852eeb4caf21fd8ceb15a1b93efd3
Markus Falk <post@markus-falk.com>
2014-08-03 16:28:27 +0200

    [Task] remind user upon adding bower package

--- addbower/index.js
+++ addbower/index.js
@@ -4,12 +4,17 @@ var yeoman = require('yeoman-generator');
 
 var AddbowerGenerator = yeoman.generators.NamedBase.extend({
 
-  // init: function () {
-  //   this.on('end', function () {
-  //     this.log('Added bower ' + this.name + ' to components/bower');
-  //   });
-  // },
+  init: function () {
+    this.pkg = this.dest.readJSON('package.json');
 
+    this.on('end', function () {
+
+      this.log('\n');
+      this.log('Added component ' + this.name + ' to components/bower/');
+      this.log('Don\'t forget to wire this into your requirejs config (components/' + this.pkg.name + '.js)');
+      this.log('\n');
+    });
+  },
   addBower: function () {
     var done = this.async();
     this.bowerInstall([this.name], { 'save': true }, done);
