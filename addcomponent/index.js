9b62fd9b0accad4a4f2c91e7744d1c9a16436c9c
Markus Falk <post@markus-falk.com>
2014-08-03 07:46:38 +0200

    [Task] add deferred module via gen

--- addcomponent/index.js
+++ addcomponent/index.js
@@ -24,25 +24,38 @@ var AddcomponentGenerator = yeoman.generators.NamedBase.extend({
 
     var prompts = [
       {
+        type: 'list',
+        name: 'ComponentType',
+        message: 'What type of module would you like to add?',
+        choices: [{
+          name: 'Standard module (compiled to main file)',
+          value: 'standardModule'
+        },
+        {
+          name: 'Deferred module (dynamically loaded)',
+          value: 'DeferredModule'
+        }]
+      },
+      {
         type: 'checkbox',
         name: 'whatFiles',
         message: 'What assets do you need?',
         choices: [
-        {
-          name: 'HTML',
-          value: 'includeHTML',
-          checked: true
-        },
-        {
-          name: 'SCSS',
-          value: 'includeSCSS',
-          checked: true
-        },
-        {
-          name: 'JS',
-          value: 'includeJS',
-          checked: true
-        }
+          {
+            name: 'HTML',
+            value: 'includeHTML',
+            checked: true
+          },
+          {
+            name: 'SCSS',
+            value: 'includeSCSS',
+            checked: true
+          },
+          {
+            name: 'JS',
+            value: 'includeJS',
+            checked: true
+          }
         ]
       }
     ];
@@ -55,32 +68,49 @@ var AddcomponentGenerator = yeoman.generators.NamedBase.extend({
       this.includeSCSS = this._hasFeature('includeSCSS');
       this.includeJS = this._hasFeature('includeJS');
 
+      this.ComponentType = answers.ComponentType;
+
       done();
     }.bind(this));
   },
 
+  setDefaults: function () {
+
+    if (this.ComponentType === 'standardModule') {
+      this.directory = 'components/app/';
+    } else {
+      this.directory = 'components/app/_deferred';
+    }
+
+  },
+
   addApp: function () {
 
-    this.mkdir('components/app/' + this.name);
 
-    if(this.includeJS) {
-      this.template('component.js', 'components/app/' + this._.slugify(this.name) + '/' + this._.slugify(this.name) + '.js');
+    this.mkdir(this.directory + this.name);
+
+    if (this.includeJS) {
+      this.template('_component.js', this.directory + this._.slugify(this.name) + '/' + this._.slugify(this.name) + '.js');
     }
-    if(this.includeSCSS) {
-      this.template('_component.scss', 'components/app/' + this._.slugify(this.name) + '/_' + this._.slugify(this.name) + '.scss');
+    if (this.includeSCSS) {
+      this.template('_component.scss', this.directory + this._.slugify(this.name) + '/_' + this._.slugify(this.name) + '.scss');
     }
-    if(this.includeHTML) {
-      this.template('component.html', 'components/app/' + this._.slugify(this.name) + '/' + this._.slugify(this.name) + '.html');
+    if (this.includeHTML) {
+      this.template('_component.html', this.directory + this._.slugify(this.name) + '/' + this._.slugify(this.name) + '.html');
     }
   },
 
   addStyling: function () {
-    if(this.includeSCSS) {
+    if (this.includeSCSS) {
 
       var path = 'components/' + this.pkg.name + '.scss',
           file = this.readFileAsString(path);
 
-      file += '@import "app/' + this._.slugify(this.name) + '/'+ this._.slugify(this.name) + '";\n';
+      if (this.ComponentType === 'standardModule') {
+        file += '@import "app/' + this._.slugify(this.name) + '/' + this._.slugify(this.name) + '";\n';
+      } else {
+        file += '@import "app/_deferred' + this._.slugify(this.name) + '/' + this._.slugify(this.name) + '";\n';
+      }
 
       this.write(path, file);
     }
