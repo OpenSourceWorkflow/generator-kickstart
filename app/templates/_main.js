621b7e44213a143af98cafb88f92f5341cad4f5e
Markus Falk <mail@markus-falk.com>
2014-08-17 15:06:19 +0200

    [Task] remove load function by default closes #29

--- app/templates/_main.js
+++ app/templates/_main.js
@@ -4,31 +4,22 @@ require([
   'accordion',<% } %>
   ], function() {
 
-  /************************************************************
-  Strict Mode
-  @see http://ejohn.org/blog/ecmascript-5-strict-mode-json-and-more
-  *************************************************************/
   'use strict';
 
-  /************************************************************
-  @description Main
-  *************************************************************/
   var Main = {
     cacheElements: function() {
       // this.$bar = $('.bar');
     },
     init: function() {
       this.cacheElements();
-      this.loadDynamicDependencies();
-    },
-    loadDynamicDependencies: function() {
-
+      // this.loadDynamicDependencies();
+    }
+    // loadDynamicDependencies: function() {
       // this.$bar.exists(function() {
       //   console.log('.bar exists: load bar');
       //   require(['assets/js/deferred/bar']);
       // });
-
-    }
+    // }
   };
 
   Main.init();
