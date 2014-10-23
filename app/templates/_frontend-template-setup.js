65b9eafd64116cc24d98e6fd10e8a5e0c2d62862
Markus Falk <falk@webit.de>
2014-10-23 15:59:59 +0200

    [Feature] add placeholder to requirejs config
    refs #12

--- app/templates/_frontend-template-setup.js
+++ app/templates/_frontend-template-setup.js
@@ -2,15 +2,19 @@ requirejs.config({
 	'appdir': '../',
   'baseUrl': './',
   'paths': {
-    'jquery': 'libs/jquery/dist/jquery.min',
-    'jquery.exists': 'libs/jquery.exists/jquery.exists',
+
+  	//{{app}}
     <% if (includeAccordion) { %>'accordion': 'libs/accordion/accordion',<% } %>
-    <% if (oldIE) { %>'respondJS': 'libs/respondJS/dest/respond.min',<% } %>
     <% if (includeAnythingSlider) { %>'slider': 'libs/anythingslider/js/jquery.anythingslider.min',<% } %>
     <% if (includeColorbox) { %>'colorbox': 'libs/colorbox/jquery.colorbox-min',<% } %>
     <% if (includeElementSwitcher) { %>'element-switcher': 'libs/element-switcher/element-switcher',<% } %>
     <% if (includeEqualHeight) { %>'jquery.equal-height': 'libs/jquery.equal-height/jquery.equal-height',<% } %>
-    <% if (includeTabs) { %>'tabs': 'libs/tabs/tabs'<% } %>
+    <% if (includeTabs) { %>'tabs': 'libs/tabs/tabs',<% } %>
+    <% if (oldIE) { %>'respondJS': 'libs/respondJS/dest/respond.min',<% } %>
+
+		//{{libs}}
+		'jquery.exists': 'libs/jquery.exists/jquery.exists',
+		'jquery': 'libs/jquery/dist/jquery.min'
   },
   'shim': {
     'jquery.exists': ['jquery'],
