83b8829b785afc076a062059f05bada7928e6fa9
Markus Falk <falk@webit.de>
2015-01-05 15:31:49 +0100

    [BUGFIX] customize modernizr task upon IE support
    refs #87

--- app/templates/_gruntfile.js
+++ app/templates/_gruntfile.js
@@ -252,8 +252,8 @@ module.exports = function(grunt) {
         "devFile" : "components/libs/modernizr-shim/modernizr.min.js",
         "outputFile" : "build/assets/js/libs/modernizr.js",
         "extra" : {
-          "shiv" : true,
-          "printshiv" : true,
+          "shiv" : <% if (oldIE) { %>true<% }  else { %>false<% } %>,
+          "printshiv" : <% if (oldIE) { %>true<% }  else { %>false<% } %>,
           "load" : false,
           "mq" : false,
           "cssclasses" : true
