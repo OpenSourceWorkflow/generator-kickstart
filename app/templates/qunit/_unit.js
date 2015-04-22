f0798bd2c0f3f370575a4dc24fbba7955002720a
Markus Falk <falk@webit.de>
2015-04-22 11:24:37 +0200

    [FEATURE] insert inital qunit test
    this ensures that qunit runs successfully on first try

--- app/templates/qunit/_unit.js
+++ app/templates/qunit/_unit.js
@@ -3,6 +3,12 @@ require([
 ],
 
 function(qunit) {
+
+  qunit.module("QUnit");
+  qunit.test("Load Qunit", function() {
+    equal(typeof(qunit), 'object', "Function should return 'Qunit Object'");
+  });
+
   // call test script here
   qunit.start();
 });
